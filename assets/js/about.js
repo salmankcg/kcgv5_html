9f651958a414327f646925942a509de25c5a9686
nazifa <nazifarashid1822@gmail.com>
2021-07-13 22:36:46 +0600

    About us development 90%

--- assets/js/about.js
+++ assets/js/about.js
@@ -2,13 +2,38 @@ $(document).ready(function(){
     new WOW().init();
   
     var swiper = new Swiper(".mySwiper", {
-        slidesPerView: "auto",
+        slidesPerView: 3,
         centeredSlides: true,
         spaceBetween: 30,
+        grabCursor: true,
+        loop: true,
+        loopFillGroupWithBlank: true,
+
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
+          dynamicBullets: true,
         },
+
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+        breakpoints: {
+            400: {
+              slidesPerView: 1,
+            },
+            600: {
+              slidesPerView: 1,
+            },
+            767: {
+              slidesPerView: 3,
+            },
+            966: {
+                slidesPerView: 3
+            }
+          }
     });
 
     $(window).scroll(function() {
