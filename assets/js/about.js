ef811d6552e51c9bcb196a38f4418552cec3e797
nazifa <nazifarashid1822@gmail.com>
2021-07-14 22:46:08 +0600

    About us page completed

--- assets/js/about.js
+++ assets/js/about.js
@@ -50,3 +50,36 @@ $(document).ready(function(){
         return false;
     });
 })
+
+
+
+var lFollowX = 0,
+    lFollowY = 0,
+    x = 0,
+    y = 0,
+    friction = 1 / 30;
+
+function moveElement() {
+  x += (lFollowX - x);
+  y += (lFollowY - y) * friction;
+  
+  translate = 'translate(' + x + 'px, ' + y + 'px) scale(1)';
+
+  $('.animated-btn').css({
+    '-webit-transform': translate,
+    '-moz-transform': translate,
+    'transform': translate
+  });
+
+  window.requestAnimationFrame(moveElement);
+}
+
+$(window).on('mousemove click', function(e) {
+
+  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+  lFollowX = (20 * lMouseX) / 100; 
+  lFollowY = (10 * lMouseY) / 100;
+});
+
+moveElement();
